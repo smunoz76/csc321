d6b494f86942e88bec6fa4434a1a59fbc6bf9934
smunoz76 <smunoz76@toromail.csudh.edu>
2024-09-23 18:33:42 -0700

    smunoz4.c

---
+++ smunoz4.c
@@ -0,0 +1,27 @@
+//Serina Munoz
+//csc-321
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+if (intvar>1){
+System.out.println("Interger variable is greater than 1.");
+}else{
+System.out.println("Interger variiable is not greater than
+1.");}
+if (doublevar < 60.0){
+System.out.println("The double variable is less than 60");
+}else{
+System.out.println("The double variable in not less than 60");
+}
+if(stringvar.equals("hello")){
+System.out.println("String variable is hello.");
+}else {
+System.out.println("String variable is not hello");}
+if (charvar !='s'){
+System.out.println("Character variable does not equat to the letter 's'.");
+else
+{
+System.out.print("String variable equals to 's'.");}
+}
